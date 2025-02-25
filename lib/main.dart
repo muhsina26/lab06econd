d61818cb6d1b46331448754d11c2bd6e344f240f
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-02-25 12:30:37 +0600

    originalNavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:lab06econd/stateful.dart';
 
 void main() {
   runApp(const Lab06());
@@ -13,44 +14,11 @@ class Lab06 extends StatelessWidget {
     return MaterialApp(
       title: 'Advanced UI Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Advance UI"),
-        ),
-        
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Profile",
-            ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              CachedNetworkImage(
-                imageUrl:
-                    "https://images.unsplash.com/photo-1573548500833-02035bc24fb6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D",
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
-              ),
-              Image.network(
-                "https://images.unsplash.com/photo-1573548500833-02035bc24fb6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D",
-              ),
-              Image.network(
-                "https://plus.unsplash.com/premium_photo-1676117274720-545b12f547fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D",
-              ),
-              Image.network(
-                "https://plus.unsplash.com/premium_photo-1674939149313-dc2933455332?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D",
-              ),
-            ],
+          appBar: AppBar(
+            title: Text("Advance UI"),
           ),
-        ),
-      ),
+          body: AppBody()
+          ),
     );
   }
 }
---
+++ lib/stateful.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppBody extends StatefulWidget {
+  const AppBody({super.key});
+
+  @override
+  State<AppBody> createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  int currentIndex = 0;
+  final List<Widget> pages = [
+    Text(
+      "Home",
+      style: TextStyle(fontSize: 100),
+    ),
+    Text("Profile"),
+  ];
+  void onClickMenu(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onClickMenu,
+        currentIndex:currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
