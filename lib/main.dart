31c23555b837d9fa32c0ca9d33d3474a557c2948
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-02-25 11:00:00 +0600

    cachedimage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,24 +11,27 @@ class Lab06 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Advanced UI',
+      title: 'Advanced UI Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Advance UI"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.network(
-                "https://unsplash.com/photos/man-in-red-and-black-jacket-standing-on-snow-covered-ground-during-daytime--1v0JL_wINc"),
-              Image.network(
-                "https://unsplash.com/photos/a-close-up-of-a-spider-man-with-glowing-eyes-PXjQaGxi4JA"),
-
-
-            ],
+          appBar: AppBar(
+            title: Text("Advance UI"),
           ),
-        )
-      ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                CachedNetworkImage(
+                  imageUrl: "http://via.placeholder.com/350x150",
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+                Image.network(
+                    "https://images.unsplash.com/photo-1573548500833-02035bc24fb6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D"),
+                Image.network(
+                    "https://plus.unsplash.com/premium_photo-1676117274720-545b12f547fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D"),
+                Image.network("https://plus.unsplash.com/premium_photo-1674939149313-dc2933455332?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D")
+              ],
+            ),
+          )),
     );
   }
 }
