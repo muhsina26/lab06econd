15a2576ac1365dd0897cd2276d12a3c4906e5bc5
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-02-25 11:46:25 +0600

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,25 +12,69 @@ class Lab06 extends StatelessWidget {
     return MaterialApp(
       title: 'Advanced UI Flutter',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Advance UI"),
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                CachedNetworkImage(
-                  imageUrl: "https://images.unsplash.com/photo-1573548500833-02035bc24fb6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D",
-                  placeholder: (context, url) => CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
+        appBar: AppBar(
+          title: Text("Advance UI"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors
+                      .blue, 
                 ),
-                Image.network(
-                    "https://images.unsplash.com/photo-1573548500833-02035bc24fb6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D"),
-                Image.network(
-                    "https://plus.unsplash.com/premium_photo-1676117274720-545b12f547fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D"),
-                Image.network("https://plus.unsplash.com/premium_photo-1674939149313-dc2933455332?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlJTIwNGt8ZW58MHx8MHx8fDA%3D")
-              ],
-            ),
-          )),
+                child: Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.spaceBetween, 
+                  children: [
+                    Icon(
+                      Icons.menu, // Left icon
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.notification_add, // Right icon
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.image),
+                title: Text('Images'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'This is a sample text widget.',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'More content can go here.',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
